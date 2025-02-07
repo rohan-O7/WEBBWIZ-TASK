96dd9ba9b84a08cdeb6233740064bf3bc5dc7fad
rohan-O7 <rohandasbbsr@gmail.com>
2025-02-07 18:34:20 +0530

    pushing the backend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
-// connect to db
+// connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
