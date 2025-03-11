a3e63b9e2ad8a53b204ba9e80c33a27100d00224
rohan-O7 <rohandasbbsr@gmail.com>
2025-03-11 18:47:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+const cors = require('cors');
 
 const app = express()
+const corsOptions = {
+  origin: process.env.LINK,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
+app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
 // middleware
 app.use(express.json())
 
